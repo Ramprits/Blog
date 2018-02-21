182f1cf10005bbe3e8fe638b43fe8f41dba00afa
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2018-02-21 16:53:00 +0530

    lodash

--- src/app/widget/view-and-content-child/view-and-content-child.component.ts
+++ src/app/widget/view-and-content-child/view-and-content-child.component.ts
@@ -20,10 +20,20 @@ export class ViewAndContentChildComponent implements OnInit, AfterViewInit {
   LiveTemplate: TemplateRef<any>;
   showViewTemplate = true;
   constructor() {}
-
+  public find: string = "barney";
   ngOnInit() {
     console.log("lodash version:", _.VERSION);
+    // console.log(_.findIndex(this.users, ["active", true]));
+    // console.log(_.concat(this.array1, this.array2));
+    // console.log(_.merge(this.object, this.other));
+    _.forEachRight(this.array1, function(params) {
+      console.log(params);
+      if (params == 6) {
+        console.log("params", params);
+      }
+    });
   }
+
   ngAfterViewInit() {
     setTimeout(() => {
       this.LiveTemplate = this.content;
@@ -34,4 +44,19 @@ export class ViewAndContentChildComponent implements OnInit, AfterViewInit {
     this.showViewTemplate = !this.showViewTemplate;
     this.LiveTemplate = this.showViewTemplate ? this.ViewChild : this.content;
   }
+  users = [
+    { user: "barney", active: false },
+    { user: "fred", active: false },
+    { user: "pebbles", active: true }
+  ];
+  array1 = [1, 2, 3, 4, 5, 6];
+  array2 = [1, 2, 3, 4, 5, 6];
+
+  object = {
+    a: [{ b: 2 }, { d: 4 }]
+  };
+
+  other = {
+    a: [{ c: 3 }, { e: 5 }]
+  };
 }
